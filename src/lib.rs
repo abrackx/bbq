9a1eb8ace72a2f32a61a3bb173130b970a662f62
alex <abrackx@gmail.com>
2021-10-18 16:15:22 -0400

    Simplification, sets up tests.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,25 @@
 extern crate generational_arena;
 
-use std::fs::File;
+use std::fs::{File, remove_file};
 use std::io::BufWriter;
 use std::path::Path;
 
 use bincode::serialize_into;
 use generational_arena::Arena;
 
-pub struct Database<'a> {
+pub struct Database {
     pub name: String,
-    pub path: &'a Path,
 }
 
-impl Database<'_> {
+impl Database {
     pub fn create(&self) {
-        let mut f = BufWriter::new(File::create(&self.path).unwrap());
+        let mut f = BufWriter::new(File::create(&self.file_name()).unwrap());
         let mut arena = Arena::new();
         let _ = arena.insert(&self.name);
         serialize_into(&mut f, &arena).unwrap();
+    }
+
+    pub fn file_name(&self) -> String {
+        format!("{}.bbq", &self.name)
     }
 }
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,29 +1,33 @@
-use std::fs::{File, remove_file};
+use std::fs::{remove_dir_all, create_dir_all};
 use std::path::Path;
 
 use bbq;
 use bbq::Database;
+use std::env::set_current_dir;
 
 #[test]
 pub fn should_create_db_file() {
-    let setup = Setup {
-        path: "test_db".into(),
-    };
-    let path = Path::new(&setup.path);
+    let dir = "test_dir".to_owned();
     let db = Database {
-        name: "test".to_string(),
-        path: path,
+        name: "test".to_owned(),
     };
+    Setup::new(&dir);
     db.create();
-    assert!(path.exists())
+    Setup::cleanup(&dir);
 }
 
 struct Setup {
     path: String,
 }
 
-impl Drop for Setup {
-    fn drop(&mut self) {
-        remove_file(&mut self.path).expect("could not remove")
+impl Setup {
+    pub fn new(path: &String) {
+        create_dir_all(path);
+        set_current_dir(path);
+    }
+
+    pub fn cleanup(path: &String) {
+        set_current_dir("..");
+        remove_dir_all(path).expect("Could not remove directory.")
     }
 }
